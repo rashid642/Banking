ba606f1d822b66727360dcfb620993b0675aabef
Md. Rashid Aziz <md.r.a.n.786@gmail.com>
2024-12-07 23:36:17 +0530

    added authetication in middle wares

--- api/account.go
+++ api/account.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
+
+	token "github.com/rashid642/banking/token"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
@@ -11,7 +14,6 @@ import (
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" binding:"required"`
 	Currency string `json:"currency" binding:"required,currency"`
 }
 
@@ -22,8 +24,10 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		return 
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload)
+
 	arg := db.CreateAccountParams {
-		Owner : req.Owner,
+		Owner : authPayload.Username,
 		Currency : req.Currency,
 		Balance : 0,
 	}
@@ -54,7 +58,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return 
 	}
-
+	
 	account, err := server.store.GetAccount(ctx, req.ID)
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -64,6 +68,14 @@ func (server *Server) getAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return 
 	}
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload) 
+
+	if account.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to this user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return 
+	}
+
 	ctx.JSON(http.StatusOK, account)
 }
 
@@ -79,7 +91,10 @@ func (server *Server) getAccountList(ctx *gin.Context) {
 		return 
 	}
 
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload) 
+
 	arg := db.ListAccountsParams {
+		Owner: authPayload.Username,
 		Limit: req.PageSize,
 		Offset: (req.PageID-1)*int32(req.PageSize),
 	}
--- api/transafer.go
+++ api/transafer.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	Database "github.com/rashid642/banking/Database/sqlc"
+	token "github.com/rashid642/banking/token"
 )
 
 type transferRequest struct {
@@ -22,10 +24,22 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	if !server.validAccount(ctx, req.FromAccountID, req.Currency) {
+
+	authPayload := ctx.MustGet(authorizationPayloadKey).(*token.Payload) 
+
+	fromAccount, valid := server.validAccount(ctx, req.FromAccountID, req.Currency)
+	if !valid{
 		return 
 	}
-	if !server.validAccount(ctx, req.ToAccountId, req.Currency) {
+
+	if fromAccount.Owner != authPayload.Username {
+		err := errors.New("account doesn't belong to this user")
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return 
+	}
+
+	_, valid = server.validAccount(ctx, req.ToAccountId, req.Currency)
+	if !valid {
 		return 
 	}
 
@@ -44,21 +58,21 @@ func (server *Server) createTransfer(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, result)
 }
 
-func (server *Server) validAccount(ctx *gin.Context, accountId int64, currecy string) bool {
+func (server *Server) validAccount(ctx *gin.Context, accountId int64, currecy string) (Database.Account, bool) {
 	account, err := server.store.GetAccount(ctx, accountId)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return false 
+			return account, false 
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-		return false 
+		return account, false 
 	}
 
 	if account.Currency != currecy {
-		err := fmt.Errorf("Account [%d] currecy mismatch %s vs %s", accountId, account.Currency, currecy) 
+		err := fmt.Errorf("account [%d] currecy mismatch %s vs %s", accountId, account.Currency, currecy) 
 		ctx.JSON(http.StatusBadRequest, errorResponse(err)) 
-		return false 
+		return account, false 
 	}
-	return true 
+	return account, true 
 }
