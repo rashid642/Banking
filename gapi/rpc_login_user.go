5bb61e0ae2106af22a476cd033487e34bd7aee20
Md. Rashid Aziz <md.r.a.n.786@gmail.com>
2024-12-17 23:15:42 +0530

    adding create user and login

---
+++ gapi/rpc_login_user.go
@@ -0,0 +1,68 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+
+	Database "github.com/rashid642/banking/Database/sqlc"
+	"github.com/rashid642/banking/pb"
+	"github.com/rashid642/banking/utils"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	// return nil, status.Errorf(codes.Unimplemented, "method LoginUser not implemented")
+
+	user, err := server.store.GetUser(ctx, req.GetUserName())
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user does not exist: %s", err)
+		}
+		return nil, status.Errorf(codes.Internal, "can not fetch user: %s", err)
+	}
+
+	err = utils.CheckPassowrd(req.Password, user.HashedPassowrd)
+	if err != nil {
+		return nil, status.Errorf(codes.Unauthenticated, "Unauthorized: %s", err)
+	}
+
+	accessToken, accessTokenPayload, err := server.tokenMaker.CreateToken(user.Username, server.config.AccessTokenDuration)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "unable to create access token: %s", err)
+	}
+
+	refreshToken, refreshTokenPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "unable to create refresh token: %s", err)
+	}
+
+	session, err := server.store.CreateSession(ctx, Database.CreateSessionParams{
+		ID:           refreshTokenPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiresAt:    refreshTokenPayload.ExpiredAt,
+	})
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "unable to create session: %s", err)
+	}
+
+	res := &pb.LoginUserResponse{
+		SessionId: session.ID.String(),
+		AccessToken: accessToken,
+		AccessTokenExpiresAt: timestamppb.New(accessTokenPayload.ExpiredAt),
+		RefreshToken: refreshToken,
+		RefreshTokenExpiresAt: timestamppb.New(refreshTokenPayload.ExpiredAt),
+		User: convertUser(user),
+	}
+	
+	return res, nil 
+
+}
